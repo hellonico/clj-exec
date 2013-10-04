d2f6e7c0b7727bda1ec8808df22f916722aa325c
Nico <hellonico@gmail.com>
2013-10-04 10:47:23 +0900

    sort map

---
+++ sort_map.clj
@@ -0,0 +1,12 @@
+#!/usr/bin/env ng-clj
+
+(def lst (read-string (first *command-line-args*)))
+(def odr (read-string (second *command-line-args*)))
+
+(defn asort [amap order]
+ (conj {} (select-keys amap order)))
+ 
+(println (asort lst odr))
+; sort_map.clj "{:a 2 :b 3}" "[:b :a]"
+ 
+ 
